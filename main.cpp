c379ab59af3d424135f99c33968c3cf5924c3d09
eleve <eleve@pc-b17-12.ec-nantes.fr>
2015-11-26 09:19:50 +0100

    first commit !

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   main.cpp
+ * Author: eleve
+ *
+ * Created on 26 novembre 2015, 08:35
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <exception>
+
+using namespace std;
+
+/*
+ Programme Hello World
+ */
+int main() {
+    cout << "Hello World !" << endl;
+
+    return 0;
+}
+
+//TODO caca
+
